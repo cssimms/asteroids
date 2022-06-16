e9f621e0de37bc050d5078927b825dfe0ee5510c
Charlie Simms <cssimms@gmail.com>
2022-06-16 12:21:12 -0700

    Serve assets with webpack dev server, though we still require the client to specify the bundle filename

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+const path = require("path");
+
 module.exports = {
   entry: "./app.js",
   mode: "development",
   output: {
-    path: `${__dirname}`,
+    path: `${__dirname}/dist`,
     filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/dist"),
+    },
+    port: 9000,
   },
 };
